a8d09a0bcb8b001f798dbd3b0bd8b8e24277c8e6
lschult <136801449+lschult@users.noreply.github.com>
2023-09-18 18:27:13 +0200

    chore: add scss

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@ import { handleSubmit, testfunction } from "./js/formHandler";
 
 import * as FormHandler from "./js/formHandler";
 
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 
