b2673017613cb8d01ae0fdd5a91aace10ace36c7
lschult <136801449+lschult@users.noreply.github.com>
2023-10-30 18:06:36 +0100

    feat: service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 // check if we need clean plugin here
 module.exports = {
   entry: "./src/client/index.js",
@@ -23,5 +27,7 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new WorkboxPlugin.GenerateSW(),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
   ],
 };
